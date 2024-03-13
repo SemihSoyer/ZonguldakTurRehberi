23a7e79e5a423fdc86de47982aa11c24bafce38a
SemihSoyer <94450489+SemihSoyer@users.noreply.github.com>
2024-03-13 16:09:04 +0300

    kodumu ekledim

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 
-export default function App() {
+const MapScreen = () => {
+  const [markers, setMarkers] = useState([]);
+  const [coordinate, setCoordinate] = useState({ latitude: 0, longitude: 0 });
+
+  const handleAddMarker = () => {
+    setMarkers([...markers, coordinate]);
+    setCoordinate({ latitude: 0, longitude: 0 });
+  };
+
+  const handleMarkerPress = (markerIndex) => {
+    // Marker tıklandığında yapılacak işlemleri burada gerçekleştirin
+    console.log('Tıklanan marker indeksi:', markerIndex);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 41.4565,
+          longitude: 31.7987,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker}
+            onPress={() => handleMarkerPress(index)}
+          />
+        ))}
+        <Polyline
+          coordinates={markers}
+          strokeWidth={3}
+          strokeColor="red"
+        />
+      </MapView>
+      <View style={{ padding: 10 }}>
+        <TextInput
+          placeholder="Latitude"
+          value={coordinate.latitude.toString()}
+          onChangeText={(text) =>
+            setCoordinate({ ...coordinate, latitude: parseFloat(text) })
+          }
+        />
+        <TextInput
+          placeholder="Longitude"
+          value={coordinate.longitude.toString()}
+          onChangeText={(text) =>
+            setCoordinate({ ...coordinate, longitude: parseFloat(text) })
+          }
+        />
+
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default MapScreen;
